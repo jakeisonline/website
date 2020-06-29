51ffa97e85dde0e209c10244f58df3a90c911880
Jake Holman <me@jakeisonline.com>
2020-06-29 13:26:02 +0100

    Rename directories css -> styles; DRY up eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -4,19 +4,24 @@ const configOptions = { dir: { input: "site", output: "_build", passthroughFileC
 // Call 11ty plugins
 const pluginSass = require("eleventy-plugin-sass");
 
+// General project constants
+const projectConfig = { stylesDir: "styles", imagesDir: "images" };
+
 module.exports = function(eleventyConfig) {
 
   // Move input directory images, but not the favicons directory
-  const imgDir = configOptions.dir.input + "/images/[!favicons]*";
+  const imgDir = configOptions.dir.input + "/" + projectConfig.imagesDir + "/[!favicons]*";
   eleventyConfig.addPassthroughCopy(imgDir);
 
   // Move input directory favicons to the output root
-  const faviconsDir = configOptions.dir.input + "/images/favicons";
+  const faviconsDir = configOptions.dir.input + "/" + projectConfig.imagesDir + "/favicons";
   eleventyConfig.addPassthroughCopy({[faviconsDir]: "./"});
 
   // Compile Sass
-  eleventyConfig.addPlugin(pluginSass, { watch: [configOptions.dir.input + '/css/*'],
-                                         outputDir: configOptions.dir.output + '/css' });
+  eleventyConfig.addPlugin(pluginSass, {
+    watch: [configOptions.dir.input + '/' + projectConfig.stylesDir + '/*'],
+    outputDir: configOptions.dir.output + '/' + projectConfig.stylesDir
+  });
 
   // Add SVG Handlebars Helper
   eleventyConfig.addHandlebarsHelper("svg", function(name) {
@@ -27,7 +32,7 @@ module.exports = function(eleventyConfig) {
     const fs = require('fs');
     const handlebars = require('handlebars');
 
-    const file = configOptions.dir.input + "/images/" + name;
+    const file = configOptions.dir.input + "/" + projectConfig.imagesDir + "/" + name;
     const content = fs.readFileSync(file, 'utf-8');
 
     return new handlebars.SafeString(content.toString());
