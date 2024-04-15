fc78ad6c3e585f4235f99aa1debb3db1085b7831
Jake Holman <me@jakeisonline.com>
2024-04-15 21:33:42 +0100

    use a sledgehammer to crack an image preloading nut

--- src/components/TaggerTag.tsx
+++ src/components/TaggerTag.tsx
@@ -1,4 +1,4 @@
-import crossIcon from "/cross.svg"
+import CrossIcon from "../../public/cross.svg?react"
 
 interface TaggerTagProps {
   label: string // String displayed as the tag's label
@@ -10,18 +10,14 @@ const TaggerTag = ({ label, removeTag }: TaggerTagProps) => {
     removeTag(label)
   }
 
-  // Preload our icon
-  const crossIconPreload = new Image()
-  crossIconPreload.src = crossIcon
-
   return (
-    <div className="bg-gray-100  rounded-lg px-2.5 py-1 flex">
+    <div className="bg-gray-100  rounded-lg pl-2.5 pr-1.5 py-1 flex">
       <span className="mr-0.5">{label}</span>
       <div
         className="flex rounded-full ml-0.5 px-1 py-0.5 opacity-55 hover:opacity-100 hover:bg-indigo-200"
         onClick={handleRemove}
       >
-        <img src={crossIconPreload.src} className="w-4" />
+        <CrossIcon className="relative w-4 h-4 top-0.5" />
       </div>
     </div>
   )
