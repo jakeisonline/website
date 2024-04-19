eed4b3d29b227a296bea3731d85a63e178806b81
Jake Holman <me@jakeisonline.com>
2024-04-20 00:21:13 +0100

    slightly nicer code styling

--- app/components/stepper/page.tsx
+++ app/components/stepper/page.tsx
@@ -3,14 +3,19 @@ import StepperField from "@/components/Stepper/StepperField"
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center w-full mx-auto text-center">
-      <p className="mb-1 text-sm text-slate-800">
-        &lt;StepperField startNum={0} minNum={0} maxNum={10} /&gt;
-      </p>
       <StepperField startNum={0} minNum={0} maxNum={10} />
-      <p className="mt-6 mb-1 text-sm text-slate-800">
-        &lt;StepperField startNum={1000} minNum={99} maxNum={1000} /&gt;
-      </p>
+      <pre className="mt-1">
+        <code className="text-slate-800 text-xs">
+          &lt;StepperField startNum={0} minNum={0} maxNum={10} /&gt;
+        </code>
+      </pre>
+      <p className="mt-8" />
       <StepperField startNum={1000} minNum={-99} maxNum={1000} />
+      <pre className="mt-1">
+        <code className="mt-2 text-slate-800 text-xs">
+          &lt;StepperField startNum={1000} minNum={99} maxNum={1000} /&gt;
+        </code>
+      </pre>
     </main>
   )
 }
