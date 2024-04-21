5a579d3c0225461741b6cf5b574bd0901a170506
Jake Holman <me@jakeisonline.com>
2024-04-21 21:34:09 +0100

    no longer use taggerfield on index

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import TaggerField from "@/components/Tagger/TaggerField"
-
 export default function Home() {
   return (
     <main className="w-full mx-auto text-center flex flex-row justify-center items-center">
-      <TaggerField></TaggerField>
+      👋
     </main>
   )
 }
