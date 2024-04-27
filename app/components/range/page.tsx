fd34012ed4c9a03a1d65160643408bdeff4d4f83
Jake Holman <me@jakeisonline.com>
2024-04-27 11:09:03 +0100

    add markup exmaple

--- app/components/range/page.tsx
+++ app/components/range/page.tsx
@@ -2,13 +2,29 @@ import RangeField from "@/components/RangeField"
 
 export default function Home() {
   return (
-    <main className="m-auto mt-10">
+    <main className="m-auto mt-10 flex w-60 justify-center items-center flex-col">
       <RangeField
         minRange={0}
         maxRange={100}
         initialLowValue={20}
         initialHighValue={80}
       />
+
+      <pre className="mt-6">
+        <code className="text-slate-800 text-xs">
+          &lt;RangeField &nbsp;&nbsp;
+          <br />
+          &nbsp;&nbsp;minRange=&#123;0&#125;
+          <br />
+          &nbsp;&nbsp;maxRange=&#123;100&#125;
+          <br />
+          &nbsp;&nbsp;initialLowValue=&#123;20&#125;
+          <br />
+          &nbsp;&nbsp;initialHighValue=&#123;80&#125;
+          <br />
+          /&gt;
+        </code>
+      </pre>
     </main>
   )
 }
