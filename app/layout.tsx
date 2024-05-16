fb0d8205c39f169139c3df923cb67bc9462ced1a
Jake Holman <me@jakeisonline.com>
2024-05-17 00:22:53 +0100

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>taggerField</title>
+        <title>Experiments</title>
       </head>
 
       <body>{children}</body>
