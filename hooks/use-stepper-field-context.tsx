ae42cca6a646d27fc773280281a2bbc4354cd380
Jake Holman <me@jakeisonline.com>
2024-07-10 21:57:36 +0100

    missed a spot

--- hooks/use-stepper-field-context.tsx
+++ hooks/use-stepper-field-context.tsx
@@ -12,30 +12,3 @@ export default function useStepperFieldContext() {
 
   return context
 }
-
-// import { useState } from "react"
-
-// const useStepperInputField = (
-//   startNum: number,
-//   minNum?: number | null,
-//   maxNum?: number | null,
-// ) => {
-//   const [stepValue, setStepValue] = useState<number>(startNum)
-
-//   const handleStep = (direction: string) => {
-//     let newValue: number
-//     if (direction === "up") {
-//       if (maxNum && stepValue >= maxNum) return
-//       newValue = stepValue + 1
-//       setStepValue(newValue)
-//     } else if (direction === "down") {
-//       if ((minNum || minNum === 0) && stepValue <= minNum) return
-//       newValue = stepValue - 1
-//       setStepValue(newValue)
-//     }
-//   }
-
-//   return { stepValue, handleStep }
-// }
-
-// export default useStepperInputField
