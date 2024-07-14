83f4c163329e68971f1a859e7fd6ce7a621143eb
Jake Holman <me@jakeisonline.com>
2024-07-14 22:40:36 +0100

    tidy up examples

--- app/components/stepper/page.tsx
+++ app/components/stepper/page.tsx
@@ -16,7 +16,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function StepperPage() {
   return (
-    <main className="container flex-1">
+    <main className="container flex-1 pb-20">
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
@@ -49,8 +49,9 @@ export default function StepperPage() {
           <Card>
             <CardContent className="pt-6 min-h-44 flex w-auto justify-center items-center">
               <StepperField
-                startNum={0}
                 minNum={0}
+                startNum={0}
+                stepShiftSize={10}
                 fieldId={"quantity"}
                 fieldName={"quantity"}
                 fieldLabel={"Quantity"}
@@ -61,8 +62,9 @@ export default function StepperPage() {
         </TabsContent>
         <TabsContent value="code">
           <CodeBlock>{`<StepperField
-  startNum={0}
   minNum={0}
+  startNum={0}
+  stepShiftSize={10}
   fieldId={"quantity"}
   fieldName={"quantity"}
   fieldLabel={"Quantity"}
@@ -164,134 +166,6 @@ export default function StepperPage() {
 </div>`}</CodeBlock>
         </TabsContent>
       </Tabs>
-      <div className="flex w-60 justify-center items-center flex-col gap-1 pb-10">
-        <StepperField startNum={0} minNum={0} maxNum={10} stepShiftSize={10} />
-        <pre className="mt-1">
-          <code className="text-slate-800 text-xs">
-            &lt;StepperField &nbsp;&nbsp;
-            <br />
-            &nbsp;&nbsp;startNum=&#123;0&#125;
-            <br />
-            &nbsp;&nbsp;minNum=&#123;0&#125;
-            <br />
-            &nbsp;&nbsp;maxNum=&#123;10&#125;
-            <br />
-            /&gt;
-          </code>
-        </pre>
-      </div>
-      <div className="flex w-60 justify-center items-center flex-col gap-1 pb-10">
-        <StepperField startNum={100} minNum={-99} maxNum={100} />
-        <pre className="mt-1">
-          <code className="mt-2 text-slate-800 text-xs">
-            &lt;StepperField
-            <br />
-            &nbsp;&nbsp;startNum=&#123;100&#125;
-            <br />
-            &nbsp;&nbsp;minNum=&#123;-99&#125;
-            <br />
-            &nbsp;&nbsp;maxNum=&#123;100&#125;
-            <br />
-            /&gt;
-          </code>
-        </pre>
-      </div>
-      <div className="flex w-60 justify-center items-center flex-col gap-1 pb-10">
-        <StepperField
-          fieldId="sizes_medium"
-          fieldName="sizes_medium"
-          fieldLabel="M"
-          fieldLabelReader="Medium"
-          startNum={100}
-          minNum={-99}
-          maxNum={100}
-        />
-        <pre className="mt-1">
-          <code className="mt-2 text-slate-800 text-xs">
-            &lt;StepperField
-            <br />
-            &nbsp;&nbsp;fieldId=&#123;&#34;sizes_medium&#34;&#125;
-            <br />
-            &nbsp;&nbsp;fieldName=&#123;&#34;sizes_medium&#34;&#125;
-            <br />
-            &nbsp;&nbsp;fieldLabel=&#123;&#34;M&#34;&#125;
-            <br />
-            &nbsp;&nbsp;fieldLabelReader=&#123;&#34;Medium&#34;&#125;
-            <br />
-            &nbsp;&nbsp;startNum=&#123;100&#125;
-            <br />
-            &nbsp;&nbsp;minNum=&#123;-99&#125;
-            <br />
-            &nbsp;&nbsp;maxNum=&#123;100&#125;
-            <br />
-            /&gt;
-          </code>
-        </pre>
-      </div>
-      <div className="flex w-60 justify-center items-center flex-col gap-1 pb-10">
-        <div className="flex gap-3">
-          <StepperField
-            collapsible={true}
-            hideBadgeNum={0}
-            fieldId="sizes_small"
-            fieldName="sizes_small"
-            fieldLabel="S"
-            fieldLabelReader="Small"
-            startNum={0}
-            minNum={0}
-            maxNum={100}
-          />
-          <StepperField
-            collapsible={true}
-            hideBadgeNum={0}
-            fieldId="sizes_med"
-            fieldName="sizes_medium"
-            fieldLabel="M"
-            fieldLabelReader="Medium"
-            startNum={3}
-            minNum={0}
-            maxNum={100}
-          />
-          <StepperField
-            collapsible={true}
-            hideBadgeNum={0}
-            fieldId="sizes_large"
-            fieldName="sizes_large"
-            fieldLabel="L"
-            fieldLabelReader="Large"
-            startNum={0}
-            minNum={0}
-            maxNum={100}
-          />
-        </div>
-        <pre className="mt-1">
-          <code className="mt-2 text-slate-800 text-xs">
-            3 *
-            <br />
-            &lt;StepperField
-            <br />
-            &nbsp;&nbsp;collapsible=&#123;true&#125;
-            <br />
-            &nbsp;&nbsp;hideBadgeNum=&#123;&#34;0&#34;&#125;
-            <br />
-            &nbsp;&nbsp;fieldId=&#123;&#34;sizes_medium&#34;&#125;
-            <br />
-            &nbsp;&nbsp;fieldName=&#123;&#34;sizes_medium&#34;&#125;
-            <br />
-            &nbsp;&nbsp;fieldLabel=&#123;&#34;M&#34;&#125;
-            <br />
-            &nbsp;&nbsp;fieldLabelReader=&#123;&#34;Medium&#34;&#125;
-            <br />
-            &nbsp;&nbsp;startNum=&#123;0&#125;
-            <br />
-            &nbsp;&nbsp;minNum=&#123;0&#125;
-            <br />
-            &nbsp;&nbsp;maxNum=&#123;100&#125;
-            <br />
-            /&gt;
-          </code>
-        </pre>
-      </div>
     </main>
   )
 }
