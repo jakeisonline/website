d31cd74315ae07873b2b5f2aa702715366a84b45
Jake Holman <me@jakeisonline.com>
2024-07-14 15:32:36 +0100

    adds shadcn and dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 import plugin from "tailwindcss/plugin"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -16,16 +26,32 @@ const config: Config = {
       },
       fontSize: {
         "2xs": "0.625rem",
-      }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [
+    require("tailwindcss-animate"),
     require("tailwindcss-inner-border"),
-    plugin(function({ addBase }) {
+    plugin(function ({ addBase }) {
       addBase({
-        "html": { fontSize: "22px" },
+        html: { fontSize: "22px" },
       })
     }),
   ],
-};
-export default config;
+} satisfies Config
+
+export default config
