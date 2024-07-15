15d60b4336ee25812a5d695df467a216aa8209db
Jake Holman <me@jakeisonline.com>
2024-07-15 21:21:41 +0100

    naughty any type

--- components/keeb.tsx
+++ components/keeb.tsx
@@ -1,6 +1,12 @@
 import { cn } from "@/lib/utils"
 
-export default function Keeb({ className, children }: any) {
+export default function Keeb({
+  className,
+  children,
+}: {
+  className?: string
+  children: React.ReactNode
+}) {
   return (
     <div
       className={cn(
