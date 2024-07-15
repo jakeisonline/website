cca920c8573f71784873689337030fe8eee0b812
Jake Holman <me@jakeisonline.com>
2024-07-15 22:58:18 +0100

    slightly increase font size

--- components/stepper-field/stepper-label.tsx
+++ components/stepper-field/stepper-label.tsx
@@ -16,7 +16,7 @@ const StepperLabel = ({
   const ariaLabel = fieldLabelReader ? fieldLabelReader : fieldLabel
 
   const labelClass = clsx(
-    "cursor-pointer pl-1.5",
+    "cursor-pointer pl-1.5 text-sm",
     collapsible && "pr-1.5",
     !collapsible && "pr-2 border-r",
     !fieldLabel && "sr-only",
