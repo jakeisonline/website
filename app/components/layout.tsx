bf9b4fff91ef188190d79eccde737f1c30c2bb91
Jake Holman <me@jakeisonline.com>
2024-07-19 22:19:50 +0100

    make SubNav dynamic

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -4,14 +4,19 @@ export const metadata = {
   title: "🛝 Playground - Components",
 }
 
-export default function ComponentsLayout({
+export default async function ComponentsLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const navItems = [
+    { name: "Range", href: "/components/range" },
+    { name: "Stepper", href: "/components/stepper" },
+  ]
+
   return (
     <div className="container flex-1 pt-6 pb-20 items-start md:grid lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-      <SubNav />
+      <SubNav title="Components" navItems={navItems} />
       <div className="relative grid xl:grid-cols-[1fr_300px]">{children}</div>
     </div>
   )
--- components/sub-nav.tsx
+++ components/sub-nav.tsx
@@ -1,17 +1,38 @@
+"use client"
+
+import { usePathname } from "next/navigation"
 import { H4 } from "./ui/headings"
 import Link from "./ui/link"
+import { cn } from "@/lib/utils"
 
-export default function SubNav() {
+type SubNavProps = {
+  title: string
+  navItems: Array<{
+    name: string
+    href: string
+  }>
+}
+
+export default function SubNav({ title, navItems }: SubNavProps) {
+  const currentPath = usePathname()
   return (
     <aside className="top-20 z-30 -ml-2 h-[calc(100vh-3.5rem)] w-full shrink-0 sticky block">
-      <H4 className="mt-0">Components</H4>
+      <H4 className="mt-0 pl-1.5">{title}</H4>
       <div className="grid gap-1 text-sm">
-        <Link href="/components/range" className="text-foreground/60">
-          Range
-        </Link>
-        <Link href="/components/stepper" className="text-foreground/60">
-          Stepper
-        </Link>
+        {navItems.map(({ name, href }) => (
+          <Link
+            key={name}
+            href={href}
+            className={cn(
+              "pl-1.5 py-1 rounded-sm",
+              currentPath === href
+                ? "text-foreground bg-accent"
+                : "text-foreground/60",
+            )}
+          >
+            {name}
+          </Link>
+        ))}
       </div>
     </aside>
   )
