db9b822b7e2f5585a527db781546b7940aa37370
Jake Holman <me@jakeisonline.com>
2024-07-19 22:39:49 +0100

    import correct link component

--- components/global-nav.tsx
+++ components/global-nav.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils"
-import Link from "next/link"
+import Link from "@/components/ui/link"
 import { usePathname } from "next/navigation"
 
 export function GlobalNav() {
@@ -26,7 +26,7 @@ export function GlobalNav() {
             "transition-colors",
             currentPath.includes(href)
               ? "text-foreground"
-              : "hover:text-foreground/80 text-foreground/60",
+              : "text-foreground/60",
           )}
         >
           {name}
