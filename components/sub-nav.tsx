b1d9964b5cf38409bbf17280d0b7edc998e498ed
Jake Holman <me@jakeisonline.com>
2024-07-24 23:01:36 +0100

    stop sub nav moving around

--- components/sub-nav.tsx
+++ components/sub-nav.tsx
@@ -16,7 +16,7 @@ type SubNavProps = {
 export default function SubNav({ title, navItems }: SubNavProps) {
   const currentPath = usePathname()
   return (
-    <aside className="top-20 z-30 -ml-2 h-[calc(100vh-3.5rem)] w-full shrink-0 sticky block">
+    <aside className="top-20 z-30 -ml-2 w-full shrink-0 sticky block">
       <H4 className="mt-0 pl-1.5">{title}</H4>
       <div className="grid gap-1 text-sm">
         {navItems.map(({ name, href }) => (
