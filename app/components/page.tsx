3d03ff39ef3c7af93c2e16b4dfc48e85a9673436
Jake Holman <me@jakeisonline.com>
2024-07-27 16:30:35 +0100

    only 2 columns for large screens

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -15,7 +15,7 @@ export default function ComponentsPage() {
   return (
     <div className="m-auto col-span-full min-h-lvh pt-10">
       <h1 className="font-semibold text-3xl">Components</h1>
-      <div className="grid grid-cols-2 gap-3 mt-3">
+      <div className="grid lg:grid-cols-2 gap-3 mt-3">
         <ComponentCard component="stepper">
           <ComponentCardHeader>Stepper</ComponentCardHeader>
           <ComponentCardContent>
