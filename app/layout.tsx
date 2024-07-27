71afd2f6702a26d016d7de9e1bd0b51d7605cbe3
Jake Holman <me@jakeisonline.com>
2024-07-27 17:02:57 +0100

    force vertical scroll bars

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,14 +19,14 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* <link
+        <link
           href="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css"
           rel="stylesheet"
-        /> */}
+        />
       </head>
 
       <body
-        className={`min-h-screen bg-background ${font.className}`}
+        className={`min-h-screen bg-background overflow-y-scroll ${font.className}`}
         suppressHydrationWarning
       >
         <ThemeContextProvider
