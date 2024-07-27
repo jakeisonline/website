b911e47f6bd060c9b380d184596587657a1e316c
Jake Holman <me@jakeisonline.com>
2024-07-27 19:35:59 +0100

    adjust component page loading skeleton layout

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -2,16 +2,22 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
   return (
-    <div className="col-span-full pt-10">
-      <div className="flex flex-col gap-3">
-        <Skeleton className="w-44 h-8" />
-        <div className="grid grid-cols-2 gap-6 mt-1.5">
-          <Skeleton className="w-full h-64" />
-          <Skeleton className="w-full h-64" />
-          <Skeleton className="w-full h-64" />
-          <Skeleton className="w-full h-64" />
+    <>
+      <div className="flex gap-6 mt-1.5">
+        <Skeleton className="w-20 h-3" />
+        <Skeleton className="w-20 h-3" />
+      </div>
+      <div className="col-span-full pt-5">
+        <div className="flex flex-col gap-3">
+          <Skeleton className="w-44 h-8" />
+          <div className="grid grid-cols-2 gap-6 mt-1.5">
+            <Skeleton className="w-full h-64" />
+            <Skeleton className="w-full h-64" />
+            <Skeleton className="w-full h-64" />
+            <Skeleton className="w-full h-64" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
