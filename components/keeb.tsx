c1f2b3d3b3929a900224de77d3c679c85547de9c
Jake Holman <me@jakeisonline.com>
2024-07-29 23:11:45 +0100

    use span for Keeb

--- components/keeb.tsx
+++ components/keeb.tsx
@@ -8,13 +8,13 @@ export default function Keeb({
   children: React.ReactNode
 }) {
   return (
-    <div
+    <span
       className={cn(
         "min-h-[30px] inline-flex justify-center items-center py-1 px-1.5 bg-page border border-gray-150 font-mono text-xs rounded-sm border-b-4 transition-border duration-100 ease-in-out hover:cursor-pointer active:border-b hover:border-primary/30",
         className,
       )}
     >
       {children}
-    </div>
+    </span>
   )
 }
