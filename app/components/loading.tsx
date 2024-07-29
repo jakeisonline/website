9e890fce4b7addafdc195d4f002d5e6d5002475b
Jake Holman <me@jakeisonline.com>
2024-07-29 21:45:36 +0100

    style adjustment

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -10,7 +10,7 @@ export default function Loading() {
       <div className="col-span-full pt-5">
         <div className="flex flex-col gap-3">
           <Skeleton className="w-44 h-8" />
-          <div className="grid grid-cols-2 gap-6 mt-1.5">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-1.5">
             <Skeleton className="w-full h-64" />
             <Skeleton className="w-full h-64" />
             <Skeleton className="w-full h-64" />
