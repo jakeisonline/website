f58cd145a1d549094ac70f090ee4fcd4dd97b938
Jake Holman <me@jakeisonline.com>
2024-08-10 00:51:15 +0100

    adds a silly waving hand

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
-
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import vercel from "@astrojs/vercel/serverless"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    react(),
+  ],
   output: "server",
-  adapter: vercel()
-});
+  adapter: vercel(),
+})
