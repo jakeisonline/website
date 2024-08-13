c53a8be0c6ba11de797d4c42d35c089ad7ab4835
Jake Holman <me@jakeisonline.com>
2024-08-13 17:03:03 +0100

    adds robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
+import robotsTxt from "astro-robots-txt"
 import vercel from "@astrojs/vercel/serverless"
 import react from "@astrojs/react"
-import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +13,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     sitemap(),
+    robotsTxt(),
     react(),
   ],
   output: "server",
