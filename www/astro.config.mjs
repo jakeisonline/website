c4148ceea9532d3b01520e80e02b281a956cb0d4
Jake Holman <me@jakeisonline.com>
2024-08-23 21:00:42 +0100

    basic layout

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
+import react from "@astrojs/react"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  output: "server",
+  adapter: vercel({
+    includeFiles: ["./public/fonts/urbanist-latin-400-normal.woff"],
+  }),
+})
---
+++ www/src/config/site.ts
@@ -0,0 +1,9 @@
+export const SITE_CONFIG = {
+  name: "jakeisonline/playground",
+  url: "https://jakeisonline.com/playground",
+  links: {
+    github: "https://github.com/jakeisonline/playground",
+  },
+}
+
+export type SiteConfig = typeof SITE_CONFIG
