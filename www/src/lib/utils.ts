1437b223d7b6b49ff2d05aea89da317d78c27132
Jake Holman <me@jakeisonline.com>
2024-08-24 15:07:33 +0100

    adds a toTitleCase util

--- www/src/lib/utils.ts
+++ www/src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function toTitleCase(str: string) {
+  return str.replace(
+    /\w\S*/g,
+    (txt) => txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase(),
+  )
+}
