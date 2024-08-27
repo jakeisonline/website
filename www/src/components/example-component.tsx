ea17a38727dd252cad31e4d1a3a9674db0bf32de
Jake Holman <me@jakeisonline.com>
2024-08-27 19:18:37 +0100

    allow code to be highlighted

--- www/src/components/example-component.tsx
+++ www/src/components/example-component.tsx
@@ -1,27 +1,48 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { codeToHtml } from "shiki"
 
 interface ExampleComponentProps {
   componentName: string
+  exampleCode: string
   children: React.ReactNode
 }
 
 export default function ExampleComponent({
-  componentName,
-  children,
+  exampleCode,
 }: ExampleComponentProps) {
+  const [html, setHtml] = useState("")
+
+  useEffect(() => {
+    const getHighlightedCode = async () => {
+      const html = await highlightCode(exampleCode)
+      setHtml(html)
+    }
+
+    getHighlightedCode()
+  }, [exampleCode, html])
+
+  if (!html) return
+
   return (
-    <>
-      <Tabs defaultValue="example">
-        <TabsList>
-          <TabsTrigger value="example">Example</TabsTrigger>
-          <TabsTrigger value="code">Code</TabsTrigger>
-        </TabsList>
-        <TabsContent value="example">Example of {componentName}</TabsContent>
-        <TabsContent value="code" className="mt-0">
-          {children}
-        </TabsContent>
-      </Tabs>
-    </>
+    <Tabs defaultValue="example">
+      <TabsList>
+        <TabsTrigger value="example">Example</TabsTrigger>
+        <TabsTrigger value="code">Code</TabsTrigger>
+      </TabsList>
+      <TabsContent value="example">{exampleCode}</TabsContent>
+      <TabsContent value="code" className="mt-0">
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </TabsContent>
+    </Tabs>
   )
+}
+
+async function highlightCode(code: string) {
+  const html = codeToHtml(code, {
+    lang: "typescript",
+    theme: "poimandres",
+  })
+
+  return html
 }
