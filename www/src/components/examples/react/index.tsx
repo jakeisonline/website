607056fca52bd1c7662bf886778b2f1ff4bf2d8f
Jake Holman <me@jakeisonline.com>
2024-08-28 20:42:16 +0100

    add stepper-shift example

--- www/src/components/examples/react/index.tsx
+++ www/src/components/examples/react/index.tsx
@@ -1,12 +1,20 @@
 import * as React from "react"
 
 export const Index: Record<string, any> = {
-  stepper: {
+  "stepper": {
     name: "stepper",
     component: React.lazy(
       () => import("@/components/examples/react/stepper-demo"),
     ),
     source: (await import(`@/components/examples/react/stepper-demo.tsx?raw`))
+      .default,
+  },
+  "stepper-shift": {
+    name: "stepper-shift",
+    component: React.lazy(
+      () => import("@/components/examples/react/stepper-shift"),
+    ),
+    source: (await import(`@/components/examples/react/stepper-shift.tsx?raw`))
       .default,
   },
 }
---
+++ www/src/components/examples/react/stepper-shift.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import {
+  StepperField,
+  StepperFieldButton,
+  StepperFieldInput,
+  StepperFieldLabel,
+} from "@/components/ui/stepper-field"
+
+export default function StepperDemo() {
+  return (
+    <StepperField min={0} shift={10} max={100}>
+      <StepperFieldLabel htmlFor="shift">Quantity</StepperFieldLabel>
+      <StepperFieldButton direction="down">-</StepperFieldButton>
+      <StepperFieldInput id="shift" />
+      <StepperFieldButton direction="up">+</StepperFieldButton>
+    </StepperField>
+  )
+}
