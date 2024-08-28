1b34130361d58b6995d869b2c807667943420f24
Jake Holman <me@jakeisonline.com>
2024-08-28 19:35:57 +0100

    better loading state for code samples

--- www/src/components/example-source.tsx
+++ www/src/components/example-source.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { Skeleton } from "./ui/skeleton"
 import { Index } from "@/components/examples/react"
 import { codeToHtml } from "shiki"
 
@@ -28,18 +29,27 @@ export default function ExampleSource({ name }: ExampleSourceProps) {
     getHighlightedCode()
   }, [name, html, rawSource])
 
-  if (!html)
-    return (
-      <div className="flex w-full rounded-lg border bg-card text-card-foreground shadow-sm min-h-44 mt-2 items-center justify-center">
-        Loading...
-      </div>
-    )
+  if (!html) return <Loading />
 
   return (
-    <div
-      style={{ marginTop: "-0.8rem" }}
-      dangerouslySetInnerHTML={{ __html: html }}
-    />
+    <Loading />
+    // <div
+    //   style={{ marginTop: "-0.8rem" }}
+    //   dangerouslySetInnerHTML={{ __html: html }}
+    // />
+  )
+}
+
+function Loading() {
+  return (
+    <div className="flex flex-col gap-1 w-full rounded-lg border bg-[#1b1e28] shadow-sm min-h-44 mt-2 p-3">
+      <Skeleton className="w-52 h-4" />
+      <Skeleton className="mt-5 w-20 h-4" />
+      <Skeleton className="ml-5 w-28 h-4" />
+      <Skeleton className="ml-5 w-28 h-4" />
+      <Skeleton className="w-28 h-4" />
+      <Skeleton className="mt-5 w-52 h-4" />
+    </div>
   )
 }
 
