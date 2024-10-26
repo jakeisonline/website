d7eee19bb9c07d9ec482e785483f1850b2e44bba
Jake Holman <me@jakeisonline.com>
2024-10-26 12:21:48 +0100

    initial cell components

--- www/src/components/examples/react/cells-demo.tsx
+++ www/src/components/examples/react/cells-demo.tsx
@@ -1,7 +1,18 @@
 import * as React from "react"
 
-import { Cells } from "@/components/ui/cells"
+import { Cell, CellRow, Cells } from "@/components/ui/cells"
 
 export default function StepperDemo() {
-  return <Cells>Cells</Cells>
+  return (
+    <Cells>
+      <CellRow>
+        <Cell name="cell_1" label="Cell 1" initialValue="Cell 1" />
+        <Cell name="cell_2" label="Cell 2" initialValue="Cell 2" />
+      </CellRow>
+      <CellRow>
+        <Cell name="cell_3" label="Cell 3" initialValue="Cell 3" />
+        <Cell name="cell_4" label="Cell 4" initialValue="Cell 4" />
+      </CellRow>
+    </Cells>
+  )
 }
--- www/src/components/ui/cells.tsx
+++ www/src/components/ui/cells.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils"
-import { forwardRef } from "react"
+import { forwardRef, useState, type HTMLInputTypeAttribute } from "react"
 
 interface CellsProps extends React.ComponentPropsWithoutRef<"div"> {
   className?: string
@@ -8,7 +8,59 @@ interface CellsProps extends React.ComponentPropsWithoutRef<"div"> {
 
 export const Cells = forwardRef<HTMLDivElement, CellsProps>(
   ({ className, children, ...props }, ref) => {
-    return <div>{children}</div>
+    return <div className={cn("", className)}>{children}</div>
   },
 )
 Cells.displayName = "Cells"
+
+interface CellRowProps extends React.ComponentPropsWithoutRef<"div"> {
+  className?: string
+  children: React.ReactNode
+}
+
+export const CellRow = forwardRef<HTMLDivElement, CellRowProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div className={cn("flex flex-row", className)} {...props} ref={ref}>
+        {children}
+      </div>
+    )
+  },
+)
+CellRow.displayName = "CellRow"
+
+interface CellProps extends React.ComponentPropsWithoutRef<"div"> {
+  type?: HTMLInputTypeAttribute
+  name: string
+  label: string
+  className?: string
+  initialValue?: string
+}
+
+export const Cell = forwardRef<HTMLInputElement, CellProps>(
+  ({ type = "text", name, label, className, initialValue, ...props }, ref) => {
+    const [value, setValue] = useState<string | undefined>(initialValue)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value)
+    }
+
+    return (
+      <>
+        <label htmlFor={name} className="sr-only">
+          {label}
+        </label>
+        <input
+          type={type}
+          name={name}
+          className={cn("px-2 py-1 w-20", className)}
+          {...props}
+          ref={ref}
+          value={value}
+          onChange={handleChange}
+        />
+      </>
+    )
+  },
+)
+Cell.displayName = "Cell"
