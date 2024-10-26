257ec97d22f9537cd4a6f8af805e64009b88f26e
Jake Holman <me@jakeisonline.com>
2024-10-26 12:43:06 +0100

    update styling

--- www/src/components/examples/react/cells-demo.tsx
+++ www/src/components/examples/react/cells-demo.tsx
@@ -6,12 +6,12 @@ export default function StepperDemo() {
   return (
     <Cells>
       <CellRow>
-        <Cell name="cell_1" label="Cell 1" initialValue="Cell 1" />
-        <Cell name="cell_2" label="Cell 2" initialValue="Cell 2" />
+        <Cell name="cell_1" label="Cell 1" initialValue="1" />
+        <Cell name="cell_2" label="Cell 2" initialValue="2" />
       </CellRow>
       <CellRow>
-        <Cell name="cell_3" label="Cell 3" initialValue="Cell 3" />
-        <Cell name="cell_4" label="Cell 4" initialValue="Cell 4" />
+        <Cell name="cell_3" label="Cell 3" initialValue="3" />
+        <Cell name="cell_4" label="Cell 4" initialValue="4" />
       </CellRow>
     </Cells>
   )
--- www/src/components/ui/cells.tsx
+++ www/src/components/ui/cells.tsx
@@ -1,14 +1,18 @@
 import { cn } from "@/lib/utils"
 import { forwardRef, useState, type HTMLInputTypeAttribute } from "react"
 
-interface CellsProps extends React.ComponentPropsWithoutRef<"div"> {
+interface CellsProps extends React.ComponentPropsWithoutRef<"form"> {
   className?: string
   children: React.ReactNode
 }
 
-export const Cells = forwardRef<HTMLDivElement, CellsProps>(
+export const Cells = forwardRef<HTMLFormElement, CellsProps>(
   ({ className, children, ...props }, ref) => {
-    return <div className={cn("", className)}>{children}</div>
+    return (
+      <form className={cn("", className)} {...props} ref={ref}>
+        {children}
+      </form>
+    )
   },
 )
 Cells.displayName = "Cells"
@@ -46,20 +50,25 @@ export const Cell = forwardRef<HTMLInputElement, CellProps>(
     }
 
     return (
-      <>
+      <div
+        className={cn(
+          "has-[:focus]:inner-border-primary has-[:focus]:inner-border-2 hover:cursor-pointer hover:inner-border-2 px-1 py-1 inner-border select-none group",
+          className,
+        )}
+      >
         <label htmlFor={name} className="sr-only">
           {label}
         </label>
         <input
           type={type}
           name={name}
-          className={cn("px-2 py-1 w-20", className)}
+          className="px-2 py-1 w-20 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none min-w-4 text-center focus:border-0 focus:outline-none cursor-pointer"
           {...props}
           ref={ref}
           value={value}
           onChange={handleChange}
         />
-      </>
+      </div>
     )
   },
 )
