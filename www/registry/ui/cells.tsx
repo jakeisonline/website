242820e837cf6657c66fb850cd05da3067cfa0b9
Jake Holman <me@jakeisonline.com>
2024-11-07 19:20:46 +0000

    refactor map to build before render, and store ref instead of values

--- www/registry/ui/cells.tsx
+++ www/registry/ui/cells.tsx
@@ -21,7 +21,7 @@ export const Cells = forwardRef<HTMLFormElement, CellsProps>(
     return (
       <CellsContextProvider>
         <CellsForm className={cn("", className)} {...props} ref={ref}>
-          {_renderCellsChildren(children)}
+          {children}
         </CellsForm>
       </CellsContextProvider>
     )
@@ -37,44 +37,58 @@ interface CellsForm extends React.ComponentPropsWithoutRef<"form"> {
 const _renderCellsChildren = (children: React.ReactElement[]) => {
   if (!children) throw new Error("No children provided to Cells")
 
-  const { cellsMap } = useCellsContext()
+  const { addRowIndex } = useCellsContext()
 
   let rowIndex = 0
-  let tmpChild
 
   return React.Children.map(children, (child) => {
-    if (!child) throw new Error("Failed to find child when iterating Cells children")
+    if (!child)
+      throw new Error("Failed to find child when iterating Cells children")
 
-    if (child.type.displayName !== "CellRow") throw new Error("Invalid child type, only CellRow is allowed")
+    if (child.type.displayName !== "CellRow")
+      throw new Error("Invalid child type, only CellRow is allowed")
 
     const tmpKey = child.key ? child.key : rowIndex
 
-    cellsMap.current.set(`row-${rowIndex.toString()}`, new Map())
+    addRowIndex(rowIndex, null)
     rowIndex++
 
     return (
-      <CellRow key={tmpKey}>{_renderCellRowChildren(rowIndex - 1, child.props.children)}</CellRow>
+      <CellRow key={tmpKey}>
+        {_renderCellRowChildren(rowIndex - 1, child.props.children)}
+      </CellRow>
     )
   })
 }
 
-const _renderCellRowChildren = (rowIndex: number, children: React.ReactElement[]) => {
+const _renderCellRowChildren = (
+  rowIndex: number,
+  children: React.ReactElement[],
+) => {
   if (!children) throw new Error("No children provided to CellRow")
 
-  const { cellsMap } = useCellsContext()
+  const { addCellIndex } = useCellsContext()
 
   let cellIndex = 0
 
   return React.Children.map(children, (child) => {
-    if (child.type.displayName !== "Cell") throw new Error("Invalid child type, only Cell is allowed")
+    if (child.type.displayName !== "Cell")
+      throw new Error("Invalid child type, only Cell is allowed")
 
-    const tmpKey = child.key ? child.key : cellIndex
+    const childRef = useRef<HTMLInputElement>(null)
 
-    cellsMap.current.get(`row-${rowIndex.toString()}`)?.set(`cell-${cellIndex.toString()}`, child.props.initialValue)
+    addCellIndex(rowIndex, cellIndex, childRef)
     cellIndex++
 
     return (
-      <Cell key={tmpKey} parentRowIndex={rowIndex} {...child.props}>{child.props.children}</Cell>
+      <Cell
+        cellIndex={cellIndex - 1}
+        parentRowIndex={rowIndex}
+        ref={childRef}
+        {...child.props}
+      >
+        {child.props.children}
+      </Cell>
     )
   })
 }
@@ -90,7 +104,7 @@ const CellsForm = forwardRef<HTMLFormElement, CellsForm>(
         {...props}
         ref={ref}
       >
-        {children}
+        {_renderCellsChildren(children)}
       </form>
     )
   },
@@ -117,32 +131,27 @@ interface CellProps extends React.ComponentPropsWithoutRef<"div"> {
   name: string
   label: string
   type?: HTMLInputTypeAttribute
+  cellIndex?: number
+  parentRowIndex?: number
   initialValue?: string
   className?: string
 }
 
 export const Cell = forwardRef<HTMLInputElement, CellProps>(
-  ({ type = "text", name, label, initialValue, className, ...props }, ref) => {
-
-    return (
-      <CellContextProvider initialValue={initialValue}>
-        <CellInput type={type} name={name} label={label} {...props} ref={ref} />
-      </CellContextProvider>
-    )
-  },
-)
-Cell.displayName = "Cell"
-
-interface CellInputProps extends React.ComponentPropsWithoutRef<"input"> {
-  name: string
-  label: string
-  type?: HTMLInputTypeAttribute
-  className?: string
-}
-
-const CellInput = forwardRef<HTMLInputElement, CellProps>(
-  ({ type = "text", name, label, className, ...props }, ref) => {
-    const { value, handleChange } = useCellContext()
+  (
+    {
+      type = "text",
+      name,
+      label,
+      initialValue,
+      cellIndex,
+      parentRowIndex,
+      className,
+      ...props
+    },
+    ref,
+  ) => {
+    const [value, setValue] = useState<string | undefined>(initialValue)
 
     const { isSelectedCell, toggleSelectedCell, clearSelectedCells } =
       useCellsContext()
@@ -171,6 +180,10 @@ const CellInput = forwardRef<HTMLInputElement, CellProps>(
       clearSelectedCells()
     }
 
+    const handleNewChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value)
+    }
+
     const isSelected = isSelectedCell(name)
 
     return (
@@ -181,12 +194,12 @@ const CellInput = forwardRef<HTMLInputElement, CellProps>(
         <input
           type={type}
           name={name}
-          className="hover:inner-border-2 focus:inner-border-primary focus:inner-border-2 inner-border data-[is-selected=true]:inner-border-primary data-[is-selected=true]:inner-border-2 w-20 min-w-4 cursor-pointer px-3 py-2 text-center [appearance:textfield] focus:outline-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+          className="w-20 min-w-4 cursor-pointer px-3 py-2 text-center inner-border [appearance:textfield] hover:inner-border-2 focus:outline-none focus:inner-border-2 focus:inner-border-primary data-[is-selected=true]:inner-border-2 data-[is-selected=true]:inner-border-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
           {...props}
           ref={ref}
           value={value}
           onKeyDown={handleKeyDown}
-          onChange={handleChange}
+          onChange={handleNewChange}
           onMouseDown={handleMouseDown}
           onClick={handleClick}
           onBlur={handleBlur}
@@ -196,55 +209,20 @@ const CellInput = forwardRef<HTMLInputElement, CellProps>(
     )
   },
 )
+Cell.displayName = "Cell"
 
-interface CellContextType {
-  value: string | undefined
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-}
-
-const CellContext = createContext<CellContextType>({
-  value: undefined,
-  handleChange: () => {},
-})
-
-const useCellContext = () => {
-  const context = useContext(CellContext)
-
-  if (!context) {
-    throw new Error("useCellContext must be used within a CellContextProvider")
-  }
-
-  return context
-}
-
-interface CellContextProviderProps {
-  initialValue?: string
-  children: React.ReactElement
-}
-
-const CellContextProvider = ({
-  initialValue,
-  children,
-}: CellContextProviderProps) => {
-  const [value, setValue] = useState<string | undefined>(initialValue)
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-  }
-
-  return (
-    <CellContext.Provider
-      value={{
-        value,
-        handleChange,
-      }}
-    >
-      {children}
-    </CellContext.Provider>
-  )
+interface CellInputProps extends React.ComponentPropsWithoutRef<"input"> {
+  name: string
+  label: string
+  type?: HTMLInputTypeAttribute
+  cellIndex?: number
+  parentRowIndex?: number
+  className?: string
 }
 
 interface CellsContextType {
+  addRowIndex: (index: number, value: any) => void
+  addCellIndex: (parentRowIndex: number, index: number, value: any) => void
   handleMouseDown?: (e: React.MouseEvent<HTMLFormElement>) => void
   isSelectedCell: (name: string) => boolean
   toggleSelectedCell: (name: string) => void
@@ -253,6 +231,8 @@ interface CellsContextType {
 }
 
 const CellsContext = createContext<CellsContextType>({
+  addRowIndex: () => {},
+  addCellIndex: () => {},
   isSelectedCell: () => false,
   toggleSelectedCell: () => {},
   clearSelectedCells: () => {},
@@ -289,6 +269,20 @@ const CellsContextProvider = ({ children }: CellsContextProviderProps) => {
     })
   }
 
+  const addRowIndex = (index: number, value: any) => {
+    cellsMap.current.set(`row-${index.toString()}`, new Map(value))
+  }
+
+  const addCellIndex = (
+    parentRowIndex: number,
+    index: number,
+    inputRef: any,
+  ) => {
+    cellsMap.current
+      .get(`row-${parentRowIndex}`)
+      ?.set(`cell-${index.toString()}`, inputRef)
+  }
+
   const isSelectedCell = (name: string) => {
     return selectedCells.includes(name)
   }
@@ -317,6 +311,8 @@ const CellsContextProvider = ({ children }: CellsContextProviderProps) => {
     <CellsContext.Provider
       value={{
         handleMouseDown,
+        addRowIndex,
+        addCellIndex,
         isSelectedCell,
         toggleSelectedCell,
         clearSelectedCells,
