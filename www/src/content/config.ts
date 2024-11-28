5188174a080e3bc10e13645176606f51b043937b
Jake Holman <me@jakeisonline.com>
2024-11-28 12:18:03 +0000

    DRY up content schema, add optionals to components schema

--- www/src/content/config.ts
+++ www/src/content/config.ts
@@ -1,30 +1,30 @@
 import { z, defineCollection } from "astro:content"
 
+const defaultSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  layout: z.string(),
+})
+
+const componentsSchema = z.object({
+  ...defaultSchema.shape,
+  isNew: z.boolean().optional().default(false),
+  isPrivate: z.boolean().optional().default(false),
+})
+
 export const componentDocsCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    component: z.string(),
-    layout: z.string(),
-  }),
+  schema: componentsSchema,
 })
 
 export const toolDocsCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    layout: z.string(),
-  }),
+  schema: defaultSchema,
 })
 
 export const generalDocsCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    layout: z.string(),
-  }),
+  schema: defaultSchema,
 })
 
 export const collections = {
