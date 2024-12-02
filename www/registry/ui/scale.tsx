11708cfe33a9ee28618bdeef0ab0f5c2e880ea3d
Jake Holman <me@jakeisonline.com>
2024-12-02 22:56:49 +0000

    early ui prototype

--- www/registry/ui/scale.tsx
+++ www/registry/ui/scale.tsx
@@ -2,6 +2,7 @@
 
 import { forwardRef } from "react"
 import { ScaleContextProvider } from "@/registry/hooks/use-scale"
+import { cn } from "@/lib/utils"
 
 interface ScaleProps extends React.ComponentPropsWithoutRef<"div"> {
   children: React.ReactElement
@@ -13,3 +14,34 @@ export const Scale = forwardRef<HTMLDivElement, ScaleProps>(
   ),
 )
 Scale.displayName = "Scale"
+
+interface ScaleStepProps extends React.ComponentPropsWithoutRef<"div"> {
+  id: string
+  label: string
+  name: string
+  isSelected?: Boolean
+}
+
+export const ScaleStep = forwardRef<HTMLDivElement, ScaleStepProps>(
+  ({ id, label, name, isSelected = false, ...props }, ref) => (
+    <div className="relative flex flex-col items-center">
+      <input
+        type="radio"
+        id={id}
+        name={name}
+        className={cn(
+          "peer h-4 w-4 appearance-none rounded-full border-4 border-background bg-blue-100 checked:border-blue-600 checked:bg-background hover:cursor-pointer",
+          isSelected && "bg-blue-600",
+        )}
+        {...props}
+      />
+      <label
+        htmlFor={id}
+        className="absolute bottom-5 text-center leading-tight hover:cursor-pointer peer-checked:font-bold"
+      >
+        {label}
+      </label>
+    </div>
+  ),
+)
+ScaleStep.displayName = "ScaleStep"
--- www/src/components/examples/react/scale-demo.tsx
+++ www/src/components/examples/react/scale-demo.tsx
@@ -1,11 +1,40 @@
 import * as React from "react"
 
-import { Scale } from "@/registry/ui/scale"
+import { Scale, ScaleStep } from "@/registry/ui/scale"
+import { cn } from "@/lib/utils"
 
 export default function ScaleDemo() {
   return (
     <Scale>
-      <p>test</p>
+      <div className="w-full">
+        {/* <div className="flex w-full justify-center">
+          <div className="flex w-11/12 justify-between">
+            <span>Very Unhappy</span>
+            <span>Unhappy</span>
+            <span>Happy</span>
+            <span>Very Happy</span>
+          </div>
+        </div> */}
+        <div className="flex min-h-8 w-full justify-center py-4">
+          <div className="relative flex h-1 w-11/12 flex-none touch-none flex-row items-center rounded-full bg-blue-100">
+            <fieldset className="absolute flex w-full justify-between">
+              <ScaleStep
+                id="very_unhappy"
+                name="satisfaction"
+                label="Very Unhappy"
+                defaultChecked
+              />
+              <ScaleStep id="unhappy" name="satisfaction" label="Unhappy" />
+              <ScaleStep id="happy" name="satisfaction" label="Happy" />
+              <ScaleStep
+                id="very_happy"
+                name="satisfaction"
+                label="Very Happy"
+              />
+            </fieldset>
+          </div>
+        </div>
+      </div>
     </Scale>
   )
 }
