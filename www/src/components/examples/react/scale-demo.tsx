8b784769dfdc61bcacf17baf0322ba3cf2664eea
Jake Holman <me@jakeisonline.com>
2024-12-03 20:51:44 +0000

    update demo

--- www/src/components/examples/react/scale-demo.tsx
+++ www/src/components/examples/react/scale-demo.tsx
@@ -1,35 +1,13 @@
 import * as React from "react"
 
-import { Scale, ScaleStep } from "@/registry/ui/scale"
-import { cn } from "@/lib/utils"
+import { Scale, ScaleFieldset, ScaleFill, ScaleStep } from "@/registry/ui/scale"
 
 export default function ScaleDemo() {
   return (
     <Scale>
-      <div className="w-full">
-        {/* <div className="flex w-full justify-center">
-          <div className="flex w-11/12 justify-between">
-            <span>Very Unhappy</span>
-            <span>Unhappy</span>
-            <span>Happy</span>
-            <span>Very Happy</span>
-          </div>
-        </div> */}
-        <div className="flex min-h-8 w-full justify-center py-4">
-          <div className="relative flex h-1 w-11/12 flex-none touch-none flex-row items-center rounded-full bg-blue-100">
-            <fieldset className="absolute flex w-full justify-between">
-              <ScaleStep id="unhappy" name="satisfaction" label="Unhappy" />
-              <ScaleStep
-                id="meh"
-                name="satisfaction"
-                label="Meh"
-                defaultChecked
-              />
-              <ScaleStep id="happy" name="satisfaction" label="Happy" />
-            </fieldset>
-          </div>
-        </div>
-      </div>
+      <ScaleStep id="unhappy" name="satisfaction" label="Unhappy" />
+      <ScaleStep id="meh" name="satisfaction" label="Meh" defaultChecked />
+      <ScaleStep id="happy" name="satisfaction" label="Happy" />
     </Scale>
   )
 }
