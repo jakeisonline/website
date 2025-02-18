21930cbdcd0754f64c75967b2c2c84b4b84c48b2
Jake Holman <me@jakeisonline.com>
2025-02-18 22:15:22 +0000

    chore: upgrade to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 import vercel from "@astrojs/vercel/static"
 import react from "@astrojs/react"
 import playformCompress from "@playform/compress"
 
+import tailwindcss from "@tailwindcss/vite"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jakeisonline.com",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    robotsTxt(),
-    react(),
-    playformCompress(),
-  ],
+
+  integrations: [robotsTxt(), react(), playformCompress()],
+
   output: "static",
+
   adapter: vercel({
     includeFiles: ["./public/fonts/urbanist-latin-400-normal.woff"],
     webAnalytics: {
       enabled: true,
     },
   }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
