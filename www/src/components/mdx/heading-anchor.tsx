13eb3fd3173773a17c598dda1055ae6ca86b8790
Jake Holman <me@jakeisonline.com>
2025-02-24 22:21:57 +0000

    add header permalink and tracking

---
+++ www/src/components/mdx/heading-anchor.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { setActiveSlug } from "@/stores/active-anchor"
+import type { FC } from "react"
+import { useEffect, useRef } from "react"
+
+const callback: IntersectionObserverCallback = (entries) => {
+  const entry = entries.find((entry) => entry.isIntersecting)
+
+  if (entry) {
+    const slug = (entry.target as HTMLAnchorElement).hash.slice(1)
+    setActiveSlug(decodeURI(slug))
+  }
+}
+
+const observer: IntersectionObserver =
+  typeof window === "undefined"
+    ? null!
+    : new IntersectionObserver(callback, {
+        rootMargin: `0% 0% -80%`,
+      })
+
+export const HeadingAnchor: FC<{ id: string }> = ({ id }) => {
+  const anchorRef = useRef<HTMLAnchorElement>(null!)
+
+  useEffect(() => {
+    const el = anchorRef.current
+    observer.observe(el)
+    return () => {
+      observer.unobserve(el)
+    }
+  }, [])
+
+  return (
+    <a
+      href={`#${id}`}
+      className="ml-1.5 text-muted-foreground no-underline opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+      aria-label="Permalink for this section"
+      ref={anchorRef}
+    >
+      #
+    </a>
+  )
+}
