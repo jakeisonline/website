09be83a8c38bf964b812401ca25651e5823b1778
Jake Holman <me@jakeisonline.com>
2025-02-25 22:13:37 +0000

    add search bar and cmdk

---
+++ www/src/components/blocks/search.tsx
@@ -0,0 +1,66 @@
+import React from "react"
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../ui/command"
+import { Button } from "../ui/button"
+import { cn } from "@/lib/utils"
+
+interface Props {
+  className?: string
+}
+
+export function Search({ className, ...props }: Props) {
+  const [open, setOpen] = React.useState(false)
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          "group relative h-8 bg-muted px-3 text-xs hover:border-foreground/80",
+          className,
+        )}
+        onClick={() => setOpen(true)}
+        {...props}
+      >
+        <span className="mr-1 hidden text-muted-foreground group-hover:text-foreground md:block">
+          Search things...
+        </span>
+        <span className="mr-1 block text-muted-foreground group-hover:text-foreground md:hidden">
+          Search...
+        </span>
+        <kbd className="hidden select-none items-center gap-0.5 rounded border bg-background px-1.5 font-mono text-2xs font-medium text-muted-foreground opacity-100 group-hover:text-foreground md:flex">
+          <span>⌘</span>
+          <span>K</span>
+        </kbd>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  )
+}
