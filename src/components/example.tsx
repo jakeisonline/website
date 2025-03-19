2bd71eb25dd355b961156060a53f5720f86beb97
Jake Holman <me@jakeisonline.com>
2025-03-19 23:26:05 +0000

    optimise post layout for mobile

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -35,13 +35,13 @@ export default function ExampleComponent({
       }
     >
       <div className="my-6">
-        <div className="ring-offset-background focus-visible:ring-ring mx-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 rounded-lg border bg-card text-card-foreground shadow-sm px-4 py-2">
+        <div className="ring-offset-background focus-visible:ring-ring md:mx-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 rounded-lg border bg-card text-card-foreground shadow-sm px-4 py-2 overflow-x-scroll">
           <div className="not-prose flex min-h-32 w-auto items-center justify-center p-6 pt-6">
             {Example}
           </div>
         </div>
         {caption && (
-          <div className="text-xs text-muted-foreground text-center px-4 mt-1.5 mx-auto">
+          <div className="text-xs text-muted-foreground text-center md:px-4 mt-1.5 mx-auto">
             {caption}
           </div>
         )}
