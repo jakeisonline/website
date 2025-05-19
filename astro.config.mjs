a7e024c42fa1b5122f6f674de3efe32488b4fc54
Jake Holman <me@jakeisonline.com>
2025-05-19 23:02:51 +0100

    add expressive code, finalise article

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
 import vercel from "@astrojs/vercel"
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers"
 import playformCompress from "@playform/compress"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import { remarkReadingTime } from "./src/lib/remark-reading-time"
+
+import expressiveCode from "astro-expressive-code"
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,12 +18,18 @@ export default defineConfig({
   trailingSlash: "never",
   prefetch: true,
   integrations: [
+    expressiveCode({
+      themes: ["plastic"],
+      plugins: [pluginLineNumbers()],
+      shiki: {
+        bundledLangs: ["javascript", "typescript", "jsx", "tsx"],
+      },
+      defaultProps: {
+        showLineNumbers: false,
+      },
+    }),
     mdx({
       remarkPlugins: [remarkReadingTime],
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "plastic",
-      },
     }),
     react(),
     sitemap(),
