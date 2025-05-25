72dfe78880e001f7ee1fca636695672f61a24176
Jake Holman <me@jakeisonline.com>
2025-05-25 16:00:38 +0100

    add pending states for initial rendering

--- src/components/ui/like.tsx
+++ src/components/ui/like.tsx
@@ -2,6 +2,7 @@ import { LikeContextProvider, useLike } from "@/hooks/use-like"
 import { SITE_CONFIG } from "@/lib/config"
 import { HeartIcon } from "lucide-react"
 import { Button } from "./button"
+import { Skeleton } from "./skeleton"
 
 interface LikeProps {
   likeId: string
@@ -10,13 +11,29 @@ interface LikeProps {
 export function Like({ likeId }: LikeProps) {
   return (
     <LikeContextProvider likeId={likeId}>
-      <LikeButton />
+      <LikeButton likeId={likeId} />
     </LikeContextProvider>
   )
 }
 
-export function LikeButton() {
-  const { totalLikes, userLikes, handleLike, atLimit } = useLike()
+export function LikeButton({ likeId }: LikeProps) {
+  const { handleLike, atLimit } = useLike()
+
+  return (
+    <Button
+      variant="outline"
+      className="p-0 hover:bg-pink-50/50 text-foreground group disabled:opacity-100"
+      onClick={handleLike}
+      disabled={atLimit}
+    >
+      <LikeAction />
+      <LikeCount />
+    </Button>
+  )
+}
+
+export function LikeAction() {
+  const { userLikes, atLimit } = useLike()
 
   const userDidLike = userLikes && userLikes > 0
 
@@ -28,30 +45,50 @@ export function LikeButton() {
 
   const userVotesLeft = SITE_CONFIG.options.maxLikes - (userLikes ?? 0)
 
-  return (
-    <Button
-      variant="outline"
-      className="p-0 hover:bg-pink-50/50 text-foreground group disabled:opacity-100"
-      onClick={handleLike}
-      disabled={atLimit}
-    >
-      <div className="flex items-center gap-2 border-r border-border h-full px-3">
+  const LikeActionDisplay = (): React.ReactNode => {
+    if (userLikes === undefined) {
+      return <Skeleton className="w-12 h-3" />
+    }
+
+    return (
+      <>
         <div className="relative h-4 w-4">
           <HeartIcon className="fill-pink-500 stroke-transparent" />
           <div
             className="w-full overflow-hidden absolute top-0 right-0 transition-all duration-300"
             style={{
-              height: `${
-                (userVotesLeft / SITE_CONFIG.options.maxLikes) * 100
-              }%`,
+              height: `${(userVotesLeft / SITE_CONFIG.options.maxLikes) * 100}%`,
             }}
           >
             <HeartIcon className="fill-pink-200 stroke-transparent h-4 w-4" />
           </div>
         </div>
         {buttonText()}
-      </div>
-      <div className="pl-0.5 pr-3">{totalLikes}</div>
-    </Button>
+      </>
+    )
+  }
+
+  return (
+    <div className="flex items-center gap-2 border-r border-border h-full px-3">
+      <LikeActionDisplay />
+    </div>
+  )
+}
+
+export function LikeCount() {
+  const { totalLikes } = useLike()
+
+  const LikeCountDisplay = (): React.ReactNode => {
+    if (totalLikes === undefined) {
+      return <Skeleton className="w-3 h-3" />
+    }
+
+    return <span>{totalLikes}</span>
+  }
+
+  return (
+    <div className="pl-0.5 pr-3">
+      <LikeCountDisplay />
+    </div>
   )
 }
